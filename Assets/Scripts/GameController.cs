af31351bd4a0c05ec04eb8f00142c1f5e3dffe77
Ayibatari Ibaba <tariibabadev@gmail.com>
2021-09-29 13:54:53 +0100

    Spawn notes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Transform lastSpawnedNote;
+    private static float noteHeight;
+    private static float noteWidth;
+    public Note notePrefab;
+    private Vector3 noteLocalScale;
+    private float noteSpawnStartPosX;
+    public float noteSpeed = 3f;
+    public const float NotesToSpawn = 20;
+    private int prevRandomIndex = -1;
+    public static GameController Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        SetDataForNoteGeneration();
+        SpawnNotes();
+    }
+
+
+    private void SetDataForNoteGeneration()
+    {
+        var topRight = new Vector3(Screen.width, Screen.height, 0);
+        var topRightWorldPoint = Camera.main.ScreenToWorldPoint(topRight);
+        var bottomLeftWorldPoint = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        var screenWidth = topRightWorldPoint.x - bottomLeftWorldPoint.x;
+        var screenHeight = topRightWorldPoint.y - bottomLeftWorldPoint.y;
+        noteHeight = screenHeight / 3;
+        noteWidth = screenWidth / 4;
+        var noteSpriteRenderer = notePrefab.GetComponent<SpriteRenderer>();
+        noteLocalScale = new Vector3(
+               noteWidth / noteSpriteRenderer.bounds.size.x * noteSpriteRenderer.transform.localScale.x,
+               noteHeight / noteSpriteRenderer.bounds.size.y * noteSpriteRenderer.transform.localScale.y, 1);
+        var leftmostPoint = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height / 2));
+        var leftmostPointPivot = leftmostPoint.x + noteWidth / 2;
+        noteSpawnStartPosX = leftmostPointPivot;
+    }
+
+    public void SpawnNotes()
+    {
+        var noteSpawnStartPosY = lastSpawnedNote.position.y + noteHeight;
+        Note note = null;
+        for (int i = 0; i < NotesToSpawn; i++)
+        {
+            var randomIndex = GetRandomIndex();
+            for (int j = 0; j < 4; j++)
+            {
+                note = Instantiate(notePrefab);
+                note.transform.localScale = noteLocalScale;
+                note.transform.position = new Vector2(noteSpawnStartPosX + noteWidth * j, noteSpawnStartPosY);
+                note.GetComponent<SpriteRenderer>().enabled = (j == randomIndex);
+            }
+            noteSpawnStartPosY += noteHeight;
+            if (i == NotesToSpawn - 1) lastSpawnedNote = note.transform;
+        }
+    }
+
+    private int GetRandomIndex()
+    {
+        var randomIndex = Random.Range(0, 4);
+        while (randomIndex == prevRandomIndex) randomIndex = Random.Range(0, 4);
+        prevRandomIndex = randomIndex;
+        return randomIndex;
+    }
+}
---
+++ Assets/Scripts/SpawnNotesTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNotesTrigger : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameController.Instance.lastSpawnedNote.gameObject)
+        {
+            GameController.Instance.SpawnNotes();
+        }
+    }
+}
